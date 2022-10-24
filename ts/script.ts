0d5db708f4dc45f67497d578b46072905e31b948
Antony <antony.alsberghe@hotmail.fr>
2022-10-24 16:59:59 +0200

    Start Exercice Chifumi

--- ts/script.ts
+++ ts/script.ts
@@ -1,11 +1,74 @@
-let compteur: number = 0;
-let divCompteur: HTMLElement | null = document.getElementById("compteur");
-let btn: HTMLElement | null = document.getElementById("btn");
-btn?.addEventListener("click", incrementCompteur);
+class Partie {
+  scores: Score = { scoreJ1: 0, scoreJ2: 0 };
+  j1: number = 0;
+  j2: number = 0;
+}
 
-function incrementCompteur() {
-  compteur++;
-  if (divCompteur) {
-    divCompteur.innerHTML = String(compteur);
+type Score = {
+  scoreJ1: number;
+  scoreJ2: number;
+};
+
+const player1Score = document.getElementById("player1Score")!;
+const player2Score = document.getElementById("player2Score")!;
+
+const pierre = document.getElementById("bPierre")!;
+const feuille = document.getElementById("bFeuille")!;
+const ciseaux = document.getElementById("bCiseaux")!;
+
+const resultat = document.getElementById("resultat")!;
+
+const imgJ1 = document.createElement("img");
+const imgJ2 = document.createElement("img");
+const pResult = document.createElement("p");
+
+const imgs = ["img/rock.svg", "img/paper.svg", "img/scissors.svg"];
+let partie1: Partie = new Partie();
+updateScore(partie1);
+
+let play = function (partie: Partie) {
+  partie.j2 = Math.ceil(Math.random() * 3);
+  if (resultat) {
+    resultat.innerHTML = "";
+    pResult.className = "";
+    imgJ1.src = imgs[partie.j1 - 1];
+    imgJ2.src = imgs[partie.j2 - 1];
+    resultat.appendChild(imgJ1);
+    resultat.append(" - ");
+    resultat.appendChild(imgJ2);
+
+    if (
+      (partie.j1 == 1 && partie.j2 == 3) ||
+      (partie.j1 == 2 && partie.j2 == 1) ||
+      (partie.j1 == 3 && partie.j2 == 2)
+    ) {
+      partie.scores.scoreJ1++;
+      pResult.classList.add("win");
+      pResult.innerText = "Gagné !";
+    } else if (partie.j1 == partie.j2) {
+      pResult.classList.add("draw");
+      pResult.innerText = "Egalité !";
+    } else {
+      partie.scores.scoreJ2++;
+      pResult.classList.add("lose");
+      pResult.innerText = "Perdu !";
+    }
+    resultat.appendChild(pResult);
+    updateScore(partie);
   }
+};
+
+pierre.addEventListener("click", function () {
+  play(partie1);
+});
+feuille.addEventListener("click", function () {
+  play(partie1);
+});
+ciseaux.addEventListener("click", function () {
+  play(partie1);
+});
+
+function updateScore(partie: Partie) {
+  player1Score.innerText = partie.scores.scoreJ1.toString();
+  player2Score.innerText = partie.scores.scoreJ2.toString();
 }
