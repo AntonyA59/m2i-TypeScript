22df037c0c7b8db842bb2c58ff349130a7f49de3
Antony <antony.alsberghe@hotmail.fr>
2022-10-24 12:25:37 +0200

    tsConfig switch to strict

--- ts/script.ts
+++ ts/script.ts
@@ -1,11 +1,11 @@
 let compteur: number = 0;
-let divCompteur: HTMLElement = document.getElementById("compteur");
-let btn: HTMLElement = document.getElementById("btn");
-btn.addEventListener("click", incrementCompteur);
+let divCompteur: HTMLElement | null = document.getElementById("compteur");
+let btn: HTMLElement | null = document.getElementById("btn");
+btn?.addEventListener("click", incrementCompteur);
 
 function incrementCompteur() {
   compteur++;
-  divCompteur.innerHTML = String(compteur);
+  if (divCompteur) {
+    divCompteur.innerHTML = String(compteur);
+  }
 }
-
-btn.addEventListener("click", incrementCompteur);
